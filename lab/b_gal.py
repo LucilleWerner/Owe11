f5d324edd0bbd4afbd636e51e56d2ab93d48637b
sevvy <heleenseverin@hotmail.com>
2018-04-17 14:58:12 +0200

    possible mutations algorithm

--- lab/b_gal.py
+++ lab/b_gal.py
@@ -9,6 +9,8 @@ https://www.ebi.ac.uk/ena/data/view/AAB18068
 
 codon_json = open('/home/sevvy/PycharmProjects/owe11/lab/codon_table.json', 'r')
 
+aacharc_json = open('/home/sevvy/PycharmProjects/owe11/lab/aa_characs.json', 'r')
+
 bgal_fasta = open('/home/sevvy/PycharmProjects/owe11/lab/ecoli_bgal.fasta', 'r')
 
 # read the sequence, split from header
@@ -24,17 +26,49 @@ codon_table = json.load(codon_json)
 # regions of interest -1 and + 1
 region_dna = ''
 region_aa = ''
-for i in range(793,805):
+for i in range(792,806):
     region_dna += codons[i]+' '
     region_aa += codon_table[codons[i]] +' '
 
 print(region_dna)
 print(region_aa)
 
-"""
-Gly795
-Asperagine for palindrome with pos - 1
-Tyrosine for palindrome with  pos + 1
-(both polar like Glycine)
+comp = {'A': 'T',
+        'T': 'A',
+        'C': 'G',
+        'G': 'C'}
 
-"""
+aa_charc = json.load(aacharc_json, 'r')
+
+alts = []
+
+# loop over the region of interest
+# find possible palindromes looking at the
+# previous codon and the next codon
+for i in range(794,804):
+    cur = codons[i]
+    cur_aa = codon_table[cur]
+
+    prev = codons[i-1]
+    prev_com = [comp[c] for c in prev]
+    prev_rev = ''.join(prev_com)[::-1]
+    prev_aa = codon_table[prev_rev]
+
+    nex = codons[i + 1]
+    nex_com = [comp[c] for c in nex]
+    nex_rev = ''.join(nex_com)[::-1]
+    nex_aa = codon_table[nex_rev]
+
+    cur_charc = aa_charc[cur_aa]
+    prev_charc = aa_charc[prev_aa]
+    nex_charc = aa_charc[nex_aa]
+
+    # if old amino acid and new amino acid have
+    # different characteristics, add to the list
+    if cur_charc != prev_charc:
+        alts.append([cur_aa+str(i), prev_aa])
+    if cur_charc != nex_charc:
+        alts.append([cur_aa+str(i), nex_aa])
+
+print(alts)
+
